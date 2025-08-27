0332a1fbdef0876b381452bbfa33d7e8ab3562ba
sgupta701 <gupta.sg003@gmail.com>
2025-08-27 23:29:01 +0530

    token refreshed

--- app/handlers/test_calendar.py
+++ app/handlers/test_calendar.py
@@ -1,5 +1,5 @@
 # delete current token_pickle and then run the test_calendar.py using python -m app.handlers.test_calendar
 
-# from app.handlers.calendar import handle_calendar
+from app.handlers.calendar import handle_calendar
 
-# print(handle_calendar("Set a reminder to wish Utkarsh a happy birthday"))
+print(handle_calendar("Set a reminder to wish Utkarsh a happy birthday"))
